d425466ca562f87df554fad253c16e07ecc8cc16
chssn <chris529@gmail.com>
2018-04-04 21:45:13 +0100

    Fix #7
    Not quite filtering but this does add a current day view and then
    historical stats below.

--- stats.php
+++ stats.php
@@ -7,10 +7,55 @@
     <div class="w3-container">
       <button onclick="window.history.go(-1); return false;" class="w3-btn w3-block w3-blue">Go Back!</button>
     </div>
+    <div class="w3-panel w3-gray w3-center">
+      <h3>Today's feed statistics</h3>
+    </div>
     <div class="w3-container">
-      <?php //section to display the amount ebm, formula and expressing that's been going on
-      //
-      ?>
+      <table class="w3-table-all">
+        <tr>
+          <th>Side</th>
+          <th>Duration</th>
+          <th>Qty of Fluid (ml)</th>
+        </tr>
+        <?php //section to display the amount ebm, formula and expressing that's been going on TODAY
+        require 'include/sql-connect.php';
+        $sql = "CREATE TEMPORARY TABLE feed_today
+        SELECT
+          (end_time - start_time) AS duration,
+          side,
+          fluid
+        FROM
+          feeding_log
+        WHERE
+          DATE(FROM_UNIXTIME(start_time)) = CURDATE();
+        SELECT
+          SEC_TO_TIME(SUM(duration)),
+          feed_side.side,
+          SUM(fluid)
+        FROM
+          feed_today
+        LEFT JOIN
+          feed_side ON feed_today.side = feed_side.id
+        GROUP BY
+          feed_side.side ASC WITH ROLLUP;";
+
+        if ($conn->multi_query($sql)) {
+          do {
+            /* store first result set */
+            if ($result = $conn->store_result()) {
+              while ($row = $result->fetch_row()) {
+                if ($row[1] == NULL) { $row[4] = "Total"; $highlight = "class='w3-pale-green'"; } else { $highlight = NULL; }
+                echo "<tr {$highlight}><td>{$row[1]}</td><td>{$row[0]}</td><td>{$row[2]}</tr></tr>";
+              }
+              $result->free();
+            }
+          } while ($conn->next_result());
+        }
+        ?>
+      </table>
+    </div>
+    <div class="w3-panel w3-gray w3-center">
+      <h3>Historical feed statistics</h3>
     </div>
     <div class="w3-container">
       <table class="w3-table-all">
@@ -21,7 +66,6 @@
           <th>Qty of Fluid (ml)</th>
         </tr>
         <?php
-        require 'include/sql-connect.php';
         $sql = "CREATE TEMPORARY TABLE feed_by_day
         SELECT
           DAYOFMONTH(FROM_UNIXTIME(start_time)) AS DAY,
@@ -44,10 +88,10 @@
         LEFT JOIN
           feed_side ON feed_by_day.side = feed_side.id
         GROUP BY
-          feed_side.side ASC,
           YEAR ASC,
           MONTH ASC,
-          DAY ASC WITH ROLLUP;";
+          DAY ASC,
+          feed_side.side ASC WITH ROLLUP;";
 
         if ($conn->multi_query($sql)) {
           do {
